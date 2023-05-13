ffaf0bb07ee0e17e0fd75cc500e7ef2ac00046dc
Alejandra <amonzon1113@gmail,com>
2023-05-13 02:43:04 -0400

    Agregado versio con costo n2

--- code/optimo.py
+++ code/optimo.py
@@ -88,3 +88,71 @@ def execute():
         roads.append((u-1,v-1,l))
 
     print(ElViaje(n,m,edges,graph,roads))
+
+
+#==============================N2===============================================
+def multi_dijkstra_array(graph, dist, n):    
+    visited = [False] * n
+
+        
+    for _ in range(n):        
+        min_dist = inf
+        min_node = None
+        for node in range(n):
+            if not visited[node] and dist[node] < min_dist:
+                min_dist = dist[node]
+                min_node = node        
+        if min_node is None:
+            break        
+        visited[min_node] = True
+                
+        for neighbor, weight in graph[min_node]:            
+            if weight > 0 and not visited[neighbor]:
+                distance = dist[min_node] + weight
+                if distance < dist[neighbor]:
+                    dist[neighbor] = distance    
+    return dist
+
+def ElViaje_n2(n, m, edges, graph, roads):
+    
+    if edges == None or edges == [] or roads == None or roads == []:
+        return 0
+
+    # Primera Parte (calculando a lo sumo n Dijkstras)
+    d = [None] * n
+    for u,v,l in roads:
+        if d[u] is None:
+            d[u] = dijkstra(graph, u, n, m)
+        if d[v] is None:
+            d[v] = dijkstra(graph, v, n, m)
+
+    # Segunda Parte (comprobando aristas útiles)
+    usefull_edge_count = 0
+    usefull_edges_mark = [[False] * n for _ in range(n)]
+
+    mark = [False] * n
+    
+    for _ in range(n):
+        v = None
+        dist = [inf] * n
+
+        for vi, ui, li in roads:
+            if v == None:
+                if not mark[vi]:
+                    v = vi
+                    mark[v] = True
+            if v == vi:
+                dist[ui] = - li
+
+        if v == None: break
+
+        dist = multi_dijkstra_array(graph, dist, n)
+
+        for x,y,w in edges:
+            if not usefull_edges_mark[x][y]:
+                if dist[x] <= -w - d[v][y] or dist[y] <= -w - d[v][x]:
+                    usefull_edge_count +=1
+                    usefull_edges_mark[x][y] = True
+                    usefull_edges_mark[y][x] = True
+
+    return usefull_edge_count
